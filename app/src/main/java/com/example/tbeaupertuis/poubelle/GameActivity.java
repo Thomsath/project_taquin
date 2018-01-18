0cc1eac9fb4f337bec1a2944122cb1fd1a511f1e
AnaisR7 <anais-r7@hotmail.fr>
2018-01-18 11:09:50 +0100

    cut image

--- app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
+++ app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
@@ -3,8 +3,11 @@ package com.example.tbeaupertuis.poubelle;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.GridView;
+import android.widget.Toast;
 
 /**
  * Created by tbeaupertuis on 18/01/18.
@@ -24,6 +27,17 @@ public class GameActivity extends AppCompatActivity {
             @Override
             public void onClick(View v){
                 time.stop();
+            }
+        });
+
+        GridView gridview = (GridView) findViewById(R.id.GridView);
+        gridview.setAdapter(new ImageAdapter(this));
+
+        gridview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View v,
+                                    int position, long id) {
+                //Toast.makeText(HelloGridView.this, "" + position,
+                        //Toast.LENGTH_SHORT).show();
             }
         });
     }
