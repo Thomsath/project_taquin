211ec98a32a46bc352e8948b9ef17d84daa3e19f
Dorian Giudice <dgiudice@info-factoryx.iut.bx1>
2018-01-18 09:36:14 +0100

    Chrono action

--- app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
+++ app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
@@ -1,8 +1,30 @@
 package com.example.tbeaupertuis.poubelle;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Chronometer;
+
 /**
  * Created by tbeaupertuis on 18/01/18.
  */
 
-public class GameActivity {
+public class GameActivity extends AppCompatActivity {
+
+    Chronometer time;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button Pause = (Button)findViewById(R.id.buttonPause);
+        Pause.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                time.stop();
+            }
+        });
+    }
 }
