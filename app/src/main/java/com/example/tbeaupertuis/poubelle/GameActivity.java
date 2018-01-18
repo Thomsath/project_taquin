29fc7612332182d41c60750903f3fe8d8a3a3c6a
Dorian Giudice <dgiudice@info-factoryx.iut.bx1>
2018-01-18 10:40:51 +0100

    Game Activity

--- app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
+++ app/src/main/java/com/example/tbeaupertuis/poubelle/GameActivity.java
@@ -1,9 +1,11 @@
 package com.example.tbeaupertuis.poubelle;
 
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Chronometer;
 
 /**
@@ -13,17 +15,42 @@ import android.widget.Chronometer;
 public class GameActivity extends AppCompatActivity {
 
     Chronometer time;
+    TextView nb_mouv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_game);
+    //Lancement du chronomètre lors de la création de l'activity
+        time = (Chronometer) findViewById(R.id.time);
+        time.start();
 
+    //Action du bouton Pause
         Button Pause = (Button)findViewById(R.id.buttonPause);
         Pause.setOnClickListener(new View.OnClickListener(){
             @Override
             public void onClick(View v){
                 time.stop();
+            }
+        });
+
+    //Action du bouton Resume
+        Button Resume = (Button)findViewById(R.id.buttonResume);
+        Resume.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                time.start();
+            }
+        });
+
+    //Action du bouton Rejouer
+        Button Rejouer = (Button)findViewById(R.id.buttonRejouer);
+        Rejouer.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+
+                time.setBase(SystemClock.elapsedRealtime());
+                time.start();
             }
         });
     }
