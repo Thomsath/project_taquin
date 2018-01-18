adf20e93741d2484e89796bf9515e7c3ff442c91
Thomas <thomas.beaupertuis@hotmail.fr>
2018-01-18 11:10:53 +0100

    Editing main activity

--- app/src/main/java/com/example/tbeaupertuis/poubelle/MainActivity.java
+++ app/src/main/java/com/example/tbeaupertuis/poubelle/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.tbeaupertuis.poubelle;
 
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -12,44 +15,76 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final View image = findViewById(R.id.imageView3);
-        // On cache l'image avant le clic
-        image.setVisibility(View.GONE);
+        final View button_start = findViewById(R.id.button_start);
+        final View img_squirrel = findViewById(R.id.img_squirrel);
+        final View img_planet = findViewById(R.id.img_planet);
 
+        // On cache l'image avant le clic
+        img_squirrel.setVisibility(View.GONE);
+        img_planet.setVisibility(View.GONE);
+        // On cache le buton start avant le clic
+        button_start.setVisibility(View.GONE);
         final View button_3x3 = findViewById(R.id.button_3x3);
         final View button_4x4 = findViewById(R.id.button_4x4);
         final View button_5x5 = findViewById(R.id.button_5x5);
         int model_grid;
-        // Au clic bouton 3x3
+
+        // ! ------------------------------------ // 3 X 3 //  ------------------------------------ !
+
         button_3x3.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 int model_grid = 3;
-                // On affiche l'image cachée
-                //image.setVisibility(View.VISIBLE);
-                button_4x4.setLeft(15);
 
+                // On affiche l'image cachée
+                button_start.setVisibility(View.GONE);
+                img_squirrel.setVisibility(View.VISIBLE);
+                img_planet.setVisibility(View.GONE);
+                button_4x4.setY(300);
             }
         });
-        // Au clic bouton 4x4
+
+        img_squirrel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                button_4x4.setY(270);
+                img_squirrel.setVisibility(View.GONE);
+
+                button_start.setY(570);
+                button_start.setVisibility(View.VISIBLE);
+            }
+        });
+
+        // ! ------------------------------------ // 4 X 4 //  ------------------------------------ !
+
         button_4x4.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 int model_grid = 4;
-                // On affiche l'image cachée
-                //image.setVisibility(View.VISIBLE);
-                button_4x4.setLeft(15);
-
+                button_4x4.setY(268);
+                button_start.setVisibility(View.GONE);
+                img_planet.setVisibility(View.VISIBLE);
+                img_squirrel.setVisibility(View.GONE);
+                button_5x5.setY(380);
             }
         });
-        // Au clic bouton 5x5
-        button_3x3.setOnClickListener(new View.OnClickListener() {
+        // Au clic de l'image 3x3
+        img_squirrel.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v){
+                //button_5x5.setY(270);
+                img_planet.setVisibility(View.GONE);
+
+                button_start.setY(570);
+                button_start.setVisibility(View.VISIBLE);
+            }
+        });
+        // ! ------------------------------------ // START //  ------------------------------------ !
+        button_start.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                int model_grid = 5;
-                // On affiche l'image cachée
-                //image.setVisibility(View.VISIBLE);
-                button_4x4.setLeft(15);
-
+                // On redirige vers l'activité de jeu
+                finish();
+                //Intent gameActivity=new Intent(this,GameActivity.class);
+                startActivity(new Intent(MainActivity.this, GameActivity.class));
+                Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
+                startActivity(gameActivity);
             }
         });
-
     }
 }
